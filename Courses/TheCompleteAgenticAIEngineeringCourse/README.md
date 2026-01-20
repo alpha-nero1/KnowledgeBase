6fdec21d56440a10788ace221ea8874ab3053eee
Ale <ale.alberga1@gmail.com>
2026-01-21 08:09:58 +1100

    Agents course progress

--- Courses/TheCompleteAgenticAIEngineeringCourse/README.md
+++ Courses/TheCompleteAgenticAIEngineeringCourse/README.md
@@ -6,3 +6,8 @@ Agent definition as of 2026
 An LLM with access to tools in a loop to acheive a goal.
 
 Remote - https://github.com/ed-donner/agents.git
+
+## To Start
+`uv venv`
+`uv pip install -r requirements.txt`
+`uv sync` to install based off the uv.lock - this one saved me!
---
+++ Projects/AiReasoningLoop/app.py
@@ -0,0 +1,123 @@
+"""
+    Code that facilitates the AI reasoning loop that we often see agents
+    undertake when responding to our questions in chatGPT and Gemini.
+
+    Courtesy of Ed Donner for the code.
+"""
+
+from rich.console import Console
+from dotenv import load_dotenv
+from openai import OpenAI
+import json
+load_dotenv(override=True)
+
+"""
+    Initialisation
+"""
+openai = OpenAI()
+todos = []
+completed = []
+
+
+def show(text):
+    try:
+        Console().print(text)
+    except Exception:
+        print(text)
+
+
+def get_todo_report() -> str:
+    result = ""
+    for index, todo in enumerate(todos):
+        if completed[index]:
+            result += f"Todo #{index + 1}: [green][strike]{todo}[/strike][/green]\n"
+        else:
+            result += f"Todo #{index + 1}: {todo}\n"
+    show(result)
+    return result
+
+
+def create_todos(descriptions: list[str]) -> str:
+    todos.extend(descriptions)
+    completed.extend([False] * len(descriptions))
+    return get_todo_report()
+
+
+def mark_complete(index: int, completion_notes: str) -> str:
+    if 1 <= index <= len(todos):
+        completed[index - 1] = True
+    else:
+        return "No todo at this index."
+    Console().print(completion_notes)
+    return get_todo_report()
+
+
+create_todos_json = {
+    "name": "create_todos",
+    "description": "Add new todos from a list of descriptions and return the full list",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "descriptions": {
+                'type': 'array',
+                'items': {'type': 'string'},
+                'title': 'Descriptions'
+                }
+                  
+            },
+        "required": ["descriptions"],
+        "additionalProperties": False
+    }
+}
+
+mark_complete_json = {
+    "name": "mark_complete",
+    "description": "Mark complete the todo at the given position (starting from 1) and return the full list",
+    "parameters": {
+        'properties': {
+            'index': {
+                'description': 'The 1-based index of the todo to mark as complete',
+                'title': 'Index',
+                'type': 'integer'
+                },
+            'completion_notes': {
+                'description': 'Notes about how you completed the todo in rich console markup',
+                'title': 'Completion Notes',
+                'type': 'string'
+                }
+            },
+        'required': ['index', 'completion_notes'],
+        'type': 'object',
+        'additionalProperties': False
+    }
+}
+
+tools = [{"type": "function", "function": create_todos_json},
+        {"type": "function", "function": mark_complete_json}]
+
+
+def handle_tool_calls(tool_calls):
+    results = []
+    for tool_call in tool_calls:
+        tool_name = tool_call.function.name
+        arguments = json.loads(tool_call.function.arguments)
+        tool = globals().get(tool_name)
+        result = tool(**arguments) if tool else {}
+        results.append({"role": "tool","content": json.dumps(result),"tool_call_id": tool_call.id})
+    return results
+
+
+def loop(messages):
+    done = False
+    while not done:
+        response = openai.chat.completions.create(model="gpt-5.2", messages=messages, tools=tools, reasoning_effort="none")
+        finish_reason = response.choices[0].finish_reason
+        if finish_reason=="tool_calls":
+            message = response.choices[0].message
+            tool_calls = message.tool_calls
+            results = handle_tool_calls(tool_calls)
+            messages.append(message)
+            messages.extend(results)
+        else:
+            done = True
+    show(response.choices[0].message.content)
